8187269a0bfc3a8b095f2a702a10608c7273bbce
Vinicius Mendite <vinicius.miff@gmail.com>
2021-02-15 23:14:11 -0300

    load categories and orders

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -54,7 +54,9 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', {
+      id,
+    });
   }
 
   useEffect(() => {
@@ -79,14 +81,19 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get('/categories');
+      setCategories(response.data);
     }
 
     loadCategories();
   }, []);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+    } else {
+      setSelectedCategory(id);
+    }
   }
 
   return (
